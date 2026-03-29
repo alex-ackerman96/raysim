4a087cc728300a35d4292382fe001acd3d0b075f
alex-a-avicena <alex.a@avicena.tech>
2026-03-29 09:50:11 -0700

    changes to material class

--- materials/material.py
+++ materials/material.py
@@ -1,8 +1,23 @@
 import json
+import pandas as pd
+import numpy as np
 
 class Material:
 
-    def __init__(self, filepath, material):
+    def __init__(self):
+        
+        self.n = np.empty(shape=(0,2), dtype=float)             # Wavelength (um) and real refractive index n
+        self.k = np.empty(shape=(0,2), dtype=float)             # Wavelength (um) and imaginary refractive index k
+        self.dielectric_const_1 = None                          
+        self.dielectric_const_2 = None
+        self.absorption = None
+        self.abbe_number = None
+        self.chromatic_dispersion = None
+        self.group_index = None
+        self.gvd = None
+        self.d = None
+
+    def import_from_json(self, filepath, material):
         
         self.filepath = filepath
         self.material = material
